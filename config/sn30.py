d261d4a032c9e6b0c4a454619f36d6310ac42d8c
Yamavu <gh@yamavu.com>
2025-08-10 10:47:41 +0200

    krita shortcuts?

--- config/sn30.py
+++ config/sn30.py
@@ -3,20 +3,23 @@ from evdev import ecodes
 DEVICE_ID = 20
 
 BTN_MAPPING = {
-    ecodes.BTN_X: ecodes.KEY_X,
-    ecodes.BTN_A: ecodes.KEY_A,
+    ecodes.BTN_X: ecodes.KEY_F,
+    ecodes.BTN_A: [ecodes.KEY_LEFTCTRL, ecodes.KEY_S],
     ecodes.BTN_B: ecodes.KEY_B,
-    ecodes.BTN_Y: ecodes.KEY_Z,
-    ecodes.BTN_TL: ecodes.KEY_L,
-    ecodes.BTN_TR: ecodes.KEY_R,
-    ecodes.BTN_SELECT: ecodes.KEY_S,
-    ecodes.BTN_START: ecodes.KEY_T,
+    ecodes.BTN_Y: ecodes.BTN_RIGHT,
+    ecodes.BTN_TL: [ecodes.KEY_LEFTCTRL, ecodes.KEY_Z],
+    ecodes.BTN_TR: [ecodes.KEY_LEFTCTRL, ecodes.KEY_LEFTSHIFT, ecodes.KEY_Z],
+    ecodes.BTN_SELECT: ecodes.KEY_LEFTCTRL,
+    ecodes.BTN_START: ecodes.KEY_LEFTALT,
 }
 
 ABS_HAT_MAPPING = {
     ecodes.ABS_X: {
+        0: [ecodes.KEY_LEFTCTRL, ecodes.KEY_Z],
+        255: [ecodes.KEY_LEFTCTRL, ecodes.KEY_LEFTSHIFT, ecodes.KEY_Z],
+    },
+    ecodes.ABS_Y: {
         0: [ecodes.KEY_LEFTCTRL, ecodes.KEY_LEFTBRACE],
         255: [ecodes.KEY_LEFTCTRL, ecodes.KEY_RIGHTBRACE],
-    },
-    ecodes.ABS_Y: {0: [ecodes.KEY_LEFT], 255: [ecodes.KEY_RIGHT]},
+    }
 }
