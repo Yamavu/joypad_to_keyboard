b1a60c42550b54e4f3007a87e6791f89d65af7de
Yamavu <gh@yamavu.com>
2025-08-11 23:47:12 +0200

    so far so good

--- config/sn30.py
+++ config/sn30.py
@@ -2,25 +2,28 @@ from evdev import ecodes
 
 DEVICE_ID = 20
 DEVICE_NAME = "8Bitdo SFC30 GamePad"
+VENDOR_ID = 11720
+PRODUCT_ID = 10288
 
 BTN_MAPPING = {
     ecodes.BTN_X: ecodes.KEY_F,
     ecodes.BTN_A: [ecodes.KEY_LEFTCTRL, ecodes.KEY_S],
     ecodes.BTN_B: ecodes.KEY_B,
-    ecodes.BTN_Y: ecodes.KEY,
-    ecodes.BTN_TL: [ecodes.KEY_LEFTCTRL, ecodes.KEY_Z],
-    ecodes.BTN_TR: [ecodes.KEY_LEFTCTRL, ecodes.KEY_LEFTSHIFT, ecodes.KEY_Z],
-    ecodes.BTN_SELECT: ecodes.KEY_LEFTCTRL,
+    ecodes.BTN_Y: ecodes.KEY_M,
+    ecodes.BTN_TL: [ecodes.KEY_LEFTCTRL, ecodes.KEY_Y],
+    ecodes.BTN_TR: [ecodes.KEY_LEFTCTRL, ecodes.KEY_LEFTSHIFT, ecodes.KEY_Y],
+    ecodes.BTN_SELECT: [ecodes.KEY_LEFTCTRL, ecodes.KEY_LEFTSHIFT, ecodes.KEY_U],
     ecodes.BTN_START: ecodes.KEY_LEFTALT,
 }
 
+INVALID_HAT_VALUE = -1
 ABS_HAT_MAPPING = {
     ecodes.ABS_X: {
-        0: [ecodes.KEY_LEFTCTRL, ecodes.KEY_Z],
-        255: [ecodes.KEY_LEFTCTRL, ecodes.KEY_LEFTSHIFT, ecodes.KEY_Z],
+        0: [ecodes.KEY_LEFTCTRL],
+        255: [ecodes.KEY_E],
     },
     ecodes.ABS_Y: {
-        0: [ecodes.KEY_LEFTCTRL, ecodes.KEY_LEFTBRACE],
-        255: [ecodes.KEY_LEFTCTRL, ecodes.KEY_RIGHTBRACE],
+        0: [ecodes.KEY_LEFTCTRL, ecodes.KEY_LEFTSHIFT, ecodes.KEY_K],
+        255: [ecodes.KEY_LEFTCTRL, ecodes.KEY_LEFTSHIFT, ecodes.KEY_L],
     }
 }
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import asyncio
 from typing import Set, Dict, Sequence
 from typing import Optional
+from collections import defaultdict
 
 import evdev
 from evdev import ecodes
 
-from config.sn30 import BTN_MAPPING, ABS_HAT_MAPPING, DEVICE_NAME, VENDOR_ID, PRODUCT_ID
+from config.sn30 import BTN_MAPPING, ABS_HAT_MAPPING, DEVICE_NAME, VENDOR_ID, PRODUCT_ID, INVALID_HAT_VALUE
 
 def map_value_to_name(value: int | Sequence[int]) -> str:
     """
@@ -62,25 +63,35 @@ class Mapper:
         self.ui = evdev.UInput()
         self.btn_mapping = BTN_MAPPING
         self.abs_mapping = ABS_HAT_MAPPING
+        self.abs_hat_pos = dict(zip(
+            ABS_HAT_MAPPING.keys(), 
+            [INVALID_HAT_VALUE] * len(ABS_HAT_MAPPING.keys())
+        ))
 
-    def send_keystroke(self, btn_code, value=None):
+
+    def send_keystroke(self, btn_code, value:int, active=True):
         if btn_code in self.btn_mapping:
             mapped_events: int | Sequence[int] = self.btn_mapping[btn_code]
-        elif btn_code in self.abs_mapping and value in self.abs_mapping[btn_code]:
+        elif btn_code in self.abs_mapping:
+            if value not in self.abs_mapping[btn_code] and self.abs_hat_pos[btn_code] != INVALID_HAT_VALUE:
+                value = self.abs_hat_pos[btn_code]
+                active = False
+            else:
+                self.abs_hat_pos[btn_code] = value
             mapped_events: int | Sequence[int] = self.abs_mapping[btn_code][value]
         else:
             mapped_events = []
         if mapped_events:
-            print(f"sending {mapped_events} to uinput")
+            print(f"{'sending' if active else 'releasing'} {map_value_to_name(mapped_events)} to uinput")
         if isinstance(mapped_events, int):
             mapped_events = [mapped_events]
         assert len(mapped_events) < 5, f"as a safety check len(mapped_events) < 5"
-        if mapped_events:
-            print(f"sending {map_value_to_name(mapped_events)} to uinput")
-        for event in mapped_events:
-            self.ui.write(ecodes.EV_KEY, event, 1)  # KEY_A down
-        for event in reversed(mapped_events):
-            self.ui.write(ecodes.EV_KEY, event, 0)  # KEY_A up
+        if active:
+            for event in mapped_events:
+                self.ui.write(ecodes.EV_KEY, event, 1)  # key down
+        else:
+            for event in reversed(mapped_events):
+                self.ui.write(ecodes.EV_KEY, event, 0)  # key up
         self.ui.syn()
     def close(self):
         self.ui.close()
@@ -99,6 +110,13 @@ def find_device(product_id, vendor_id, name) -> Optional[evdev.InputDevice]:
     for device in devices:
         print(device.path, device.name, device.info.vendor, device.info.product)
 
+def map_button_press(event, mapper):
+    print(evdev.categorize(event), repr(event))
+    _active = event.value == 1
+    mapper.send_keystroke(event.code, event.value, active=_active)
+
+def map_hat_movement(event, mapper):
+    mapper.send_keystroke(event.code, event.value)
 
 async def main():
     mapper = Mapper()
@@ -109,18 +127,15 @@ async def main():
             return
         async for event in input_device.async_read_loop():
             event: evdev.InputEvent = event
-            if event.type == ecodes.EV_KEY and event.value == 1:
-                print(evdev.categorize(event), repr(event))
-                mapper.send_keystroke(event.code)
+            if event.type == ecodes.EV_KEY:
+                map_button_press(event, mapper)
             elif event.type == ecodes.EV_ABS:
                 print(evdev.categorize(event), repr(event))
-                mapper.send_keystroke(event.code, event.value)
+                map_hat_movement(event, mapper)
     except PermissionError:
         print ("no permission for /dev/input")
     finally:
         mapper.close()
-
-
 
 if __name__ == "__main__":
     asyncio.run(main())
